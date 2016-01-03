5385befc4358f6a00422b055e43574bdba261971
Jamie King <king0120@gmail.com>
2016-01-03 11:09:30 -0500

    Fixed API key problems

--- js/index.js
+++ js/index.js
@@ -7,12 +7,12 @@ $(document).ready(function() {
   var $tStyle = " Fahrenheit";
   var $weatherPic;
   var $currentCity;
-
 $.getJSON("http://ipinfo.io/json", function(response) {
-    //Current location info from ipingo
+    //Current location info from ipinfo
     console.log(response.city, response.country);
     $currentCity = response.city;
     $.getJSON('http://api.openweathermap.org/data/2.5/weather', {
+        APPID: "03d186981605d1e062c234348033eefe",
         q: $currentCity,
         units: $units
       },
@@ -54,15 +54,15 @@ $.getJSON("http://ipinfo.io/json", function(response) {
             $('body').css("background-image", "");
             $('body').css("background-image", 'url(http://www.4cleanair.org/sites/default/files/committees_items/clear-sky.jpg)');
 
-        };
+        }
 
         if (parseInt($weatherMain) >= 80) {
           $('#tempPic').html("<img src='http://i.imgur.com/IGfnCQN.png'>");
         } else if (parseInt($weatherMain) >= 32) {
-          $('#tempPic').html("<img src='http://i.imgur.com/c1baR7a.png'>")
+          $('#tempPic').html("<img src='http://i.imgur.com/c1baR7a.png'>");
         } else {
-          $('#tempPic').html("<img src='http://i.imgur.com/EofQ8sd.png'>")
-        }; //end temp pic
+          $('#tempPic').html("<img src='http://i.imgur.com/EofQ8sd.png'>");
+        }//end temp pic
 
       });
   });
@@ -87,7 +87,7 @@ $.getJSON("http://ipinfo.io/json", function(response) {
   $('form').submit(function(event) {
     event.preventDefault(); //keeps page from reloading
     var $search = $('#search');
-    var weatherAPI = 'http://api.openweathermap.org/data/2.5/weather';
+    var weatherAPI = 'http://api.openweathermap.org/data/2.5/weather?APPID=03d186981605d1e062c234348033eefe';
 
     //data used is City, Country.  Want to add optional Zip Code or City ID
     var data = {
@@ -114,23 +114,23 @@ $.getJSON("http://ipinfo.io/json", function(response) {
         if (parseInt($weatherMain) >= 80) {
           $('#tempPic').html("<img src='http://i.imgur.com/IGfnCQN.png'>");
         } else if (parseInt($weatherMain) >= 32) {
-          $('#tempPic').html("<img src='http://i.imgur.com/c1baR7a.png'>")
+          $('#tempPic').html("<img src='http://i.imgur.com/c1baR7a.png'>");
         } else {
-          $('#tempPic').html("<img src='http://i.imgur.com/EofQ8sd.png'>")
-        };
+          $('#tempPic').html("<img src='http://i.imgur.com/EofQ8sd.png'>");
+        }
       } else {
         if (parseInt($weatherMain) >= 25) {
           $('#tempPic').html("<img src='http://i.imgur.com/IGfnCQN.png'>");
         } else if (parseInt($weatherMain) >= 0) {
-          $('#tempPic').html("<img src='http://i.imgur.com/c1baR7a.png'>")
+          $('#tempPic').html("<img src='http://i.imgur.com/c1baR7a.png'>");
         } else {
-          $('#tempPic').html("<img src='http://i.imgur.com/EofQ8sd.png'>")
-        };
+          $('#tempPic').html("<img src='http://i.imgur.com/EofQ8sd.png'>");
+        }
       }
 
       //Changes background depending on weather conditions.  add more else if statements
       if ($weatherDescription === "Clouds") {
-        $('body').css("background-image", "url('http://i.imgur.com/JaajGKM.jpg')")
+        $('body').css("background-image", "url('http://i.imgur.com/JaajGKM.jpg')");
       } else if ($weatherDescription === "Rain") {
         $('body').css("background-image", "");
         $('body').css("background-image", 'url(http://cdni.wired.co.uk/1240x826/s_v/Storm-2--Wired-19sep13_REX_Marko-Korosec_b_1240x826.jpg)');
@@ -139,16 +139,16 @@ $.getJSON("http://ipinfo.io/json", function(response) {
         $('body').css("background-image", 'url(http://img07.deviantart.net/ea88/i/2012/198/6/b/drizzle__by_niki91-d57kcpt.jpg)');
       } else if ($weatherDescription === "Thunderstorm") {
         $('body').css("background-image", "");
-        $('body').css("background-image", 'url(https://www.ngdc.noaa.gov/hazard/icons/quiz/thunderstorms.jpg)')
+        $('body').css("background-image", 'url(https://www.ngdc.noaa.gov/hazard/icons/quiz/thunderstorms.jpg)');
       } else if ($weatherDescription === "Snow") {
         $('body').css("background-image", "");
-        $('body').css("background-image", 'url(        https://upload.wikimedia.org/wikipedia/commons/4/4a/Snow_on_the_mountains_of_Southern_California.jpg)')
+        $('body').css("background-image", 'url(        https://upload.wikimedia.org/wikipedia/commons/4/4a/Snow_on_the_mountains_of_Southern_California.jpg)');
       } else {
         $('body').css("background-image", "");
         $('body').css("background-image", 'url(http://www.4cleanair.org/sites/default/files/committees_items/clear-sky.jpg)');
       }
 
-    };
+    }
     //process the JSON
     $.getJSON(weatherAPI, data, showWeather);
 
